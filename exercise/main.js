705d075d6a0e079ec8889e9d786334fd5264ba1c
manuquirozy <manuquirozy@gmail.com>
2018-10-11 14:43:51 -0500

    can click on favorites

--- exercise/main.js
+++ exercise/main.js
@@ -1,20 +1,32 @@
 let a = fetch('https://randomuser.me/api/?results=100');
-let b= a.then(function(resultados){
-    return resultados.json();
+let b= a.then(function(results){
+    return results.json();
 }). then(here=>{
     const authors=here.results;
     const miSection=document.querySelector('section');
+    const miAside=document.querySelector('aside');
     authors.forEach(author => {
-        const figCap=document.createElement('figcaption');
-        const img=document.createElement('img');
         const article=document.createElement('article');
+        const fig= document.createElement('figure');
+        const img=document.createElement('img');    
+        const figCap=document.createElement('figcaption');    
         img.src=author.picture.large;
         figCap.innerHTML=author.name.first+' '+author.name.last;  
         miSection.append(article);      
-        article.append(img);
-        article.append(figCap);
-        article.addEventListener('click', ()=>{
-            console.log("click");
+        article.append(fig);
+        fig.append(img);
+        fig.append(figCap);
+        article.addEventListener('click', (click)=>{
+            console.log(click);
+            const favarticle= document.createElement('article');
+            const favfig= document.createElement('figure');
+            const favimg=document.createElement('img');
+            const favfigCap=document.createElement('figcaption');  
+            favfig.innerHTML=click.path[0].outerHTML; 
+            favfigCap.innerHTML=click.path[1].innerText;   
+            miAside.append(favarticle);
+            favarticle.append(favfig);                
+            favfig.append(favfigCap);                         
         })
     });
 });
