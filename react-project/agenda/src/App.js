2375928052913913a442bf71c5d7909bbbd8873b
manuquirozy <manuquirozy@gmail.com>
2018-10-19 18:27:08 -0500

    contact list

--- react-project/agenda/src/App.js
+++ react-project/agenda/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import city from './city-wallpaper-11.jpg';
 
 class App extends Component {
   constructor(props){
@@ -27,8 +28,11 @@ class App extends Component {
 
 const Home=(props)=>{
   return(
-    <div>
-      Hello, this is my Home
+    <div className="containerhome">
+    <div className="welcome">
+      Welcome
+    </div> 
+    <div className="press">Press contact list to view all your contacts</div>   
     </div>
   );
 }
@@ -36,7 +40,7 @@ const Home=(props)=>{
 const Navigation=(props)=>{
   return(
     <nav>
-      <ul>
+      <ul className="navigation">
         <li><Link to={`/`}>Home</Link></li>
         <li><Link to={`/contactlist`}>Contact List</Link></li>
       </ul>
@@ -46,9 +50,9 @@ const Navigation=(props)=>{
 
 const ContactList = (props)=>{
   return (
-    <div>
-      <h2>{props.title}</h2>
-      {props.contacts.map(contact => <ContactCard key={contact.name.first} contact={contact} addToFavorites={props.addToFavorites}/>)}      
+    <div className="contact-list">
+      <h2 className="title">{props.title}</h2>
+      {props.contacts.map(contact => <ContactCard key={contact.name.first} contact={contact} addToFavorites={props.addToFavorites} deleteContact={props.deleteContact}/>)}      
     </div>
     
   );
@@ -69,21 +73,26 @@ class ContactCard extends Component {
   constructor(props){
     super(props);
     this.onClickFavorites=this.onClickFavorites.bind(this);
+    this.onClickDelete=this.onClickDelete.bind(this);
   }
 
   onClickFavorites(){
     this.props.addToFavorites(this.props.contact);
   }
 
+  onClickDelete(){
+    this.props.deleteContact(this.props.contact);
+  }
+
   render(){
     return(
       <div className="contact-card"> 
         <figure>
-          <img src={this.props.contact.picture.medium} alt="Autor"/>
-          <figcaption>{this.props.contact.name.first}</figcaption>
+          <img className="author" src={this.props.contact.picture.medium} alt="Autor"/>
+          <figcaption>{this.props.contact.name.first} {this.props.contact.name.last}</figcaption>
         </figure>
         <button onClick={this.onClickFavorites}>Favorito</button>
-        <button>Eliminar</button>
+        <button onClick={this.onClickDelete}>Eliminar</button>
       </div>
     );
   }
@@ -98,18 +107,43 @@ class ContactListSection extends Component {
     };
   
     this.addToFavorites=this.addToFavorites.bind(this);
+    this.deleteContact=this.deleteContact.bind(this);
   }
   
   addToFavorites(contact){
-    console.log("Añadir a favoritos", contact);
-    const newAll=this.state.all.filter(c => c.email!==contact.email);
-    const newFavorites=this.state.favorites.concat(contact);
-    this.setState({
-      all:newAll,
-      favorites: newFavorites
-    });
+    if(this.state.all.indexOf(contact)===-1){
+      const newFavorites=this.state.favorites.filter(c => c.email!==contact.email);
+      const newAll=this.state.all.concat(contact);
+      this.setState({
+        all:newAll,
+        favorites: newFavorites
+      });
+    }
+    else{
+      const newAll=this.state.all.filter(c => c.email!==contact.email);
+      const newFavorites=this.state.favorites.concat(contact);
+      this.setState({
+        all:newAll,
+        favorites: newFavorites
+      });
+    }
   }
-  
+
+  deleteContact(contact){
+    if(this.state.all.indexOf(contact)===-1){
+      const newFavorites=this.state.favorites.filter(c => c.email!==contact.email);
+      this.setState({
+        favorites: newFavorites
+      });
+    }
+    else{
+      const newAll=this.state.all.filter(c => c.email!==contact.email);
+      this.setState({
+        all:newAll
+      });
+    }
+  }
+
   componentDidMount(){
     fetch("https://randomuser.me/api/?results=10")
       .then(results => results.json())
@@ -129,8 +163,8 @@ class ContactListSection extends Component {
     render() {
       return (
         <div className="App">
-          <ContactList contacts= {this.state.all} title="Todos" key="1" addToFavorites={this.addToFavorites}/>
-          <ContactList contacts= {this.state.favorites} title="Favoritos" key="2" addToFavorites={this.addToFavorites}/>
+          <ContactList className="all" contacts= {this.state.all} title="All Contacts" key="1" addToFavorites={this.addToFavorites} deleteContact={this.deleteContact}/>
+          <ContactList className="favorites" contacts= {this.state.favorites} title="Favorites" key="2" addToFavorites={this.addToFavorites} deleteContact={this.deleteContact}/>
         </div>
       );
     }
